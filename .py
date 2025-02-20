74827150d987c20fa2e0a81b5e2b773e14b04d11
Bathula Rushmi Lohitha <162041451+RushmiLohitha@users.noreply.github.com>
2025-02-20 21:04:20 +0530

    Python.py
    Basic Python Concepts

---
+++ .py
@@ -0,0 +1,63 @@
+#print single number
+print(2)
+
+#print multiple number
+print(1,2,3,4,5)
+
+#print with out variables
+print(1+6)#addition
+
+print(2-4)#subtraction
+
+print(39*7)#multiplication
+
+print(27/3)#division
+
+print(7**2)#exponentiation
+
+print(4//3)#floor division = it prints only integer value
+
+#by using variables for arthematic operations
+n_1 = 4
+n_2 = 5
+
+print(n_1+n_2)
+
+print(n_1-n_2)
+
+print(n_1*n_2)
+
+print(n_1/n_2)
+
+print(n_1**n_2)
+
+print(n_1//n_2)
+
+#string
+name="Bathula Rushmi Lohitha"
+
+print(n)
+
+#type method is used to find the which data type like string,int,float.
+print(type(n))
+
+#len() its prints the length of the string
+print(len(n))
+
+#index[] its prints index of string
+print(n[5])
+
+print(n[-1])#print index from reverse
+
+#slicing: Python, [:] is a slice notation that creates a shallow copy of the entire sequence.
+print(n[0:10])
+
+print(n[:])
+
+print(n[0:10])
+
+print(n[:8])
+
+print(n[0:9:1])#step
+
+print(n[-10:-3])
